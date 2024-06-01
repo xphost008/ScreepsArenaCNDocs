0380fae4986cb36456543f76133bf1a1fbd7c9b7
xphost <273020451@qq.com>
2024-06-02 03:31:55 +0800

    fix search box!!

--- js/catalog.js
+++ js/catalog.js
@@ -1,7 +1,23 @@
+let raw_body;
+function load(){
+    raw_body = document.getElementsByClassName("content-right")[0].innerHTML
+}
 function search() {
     let search_text = document.getElementById("search-box").value
-    let bodp = document.getElementsByClassName("content-right")[0].innerHTML
-    let re = new RegExp(search_text, "gi")
-    // document.getElementsByClassName("content-right")[0].innerHTML = bodp.replace("<span class=\"hlt\">", "")
-    document.getElementsByClassName("content-right")[0].innerHTML = bodp.replace(re, `<span class="hlt">${search_text}</span>`)
+    document.getElementsByClassName("content-right")[0].innerHTML = raw_body
+    var regex = new RegExp(search_text, 'gi');
+
+    var textNodes = document.createTreeWalker(document.getElementsByClassName("content-right")[0], NodeFilter.SHOW_TEXT, null, false);
+    let nodeToReplace = []
+    while(node = textNodes.nextNode()) {
+        if (node.textContent.includes(search_text)) {
+            nodeToReplace.push(node)
+        }
+    }
+    nodeToReplace.forEach(function(node){
+        var hld = node.textContent.replace(regex, '<span class="hlt">' + search_text + "</span>")
+        var newNode = document.createElement('span')
+        newNode.innerHTML = hld
+        node.parentNode.replaceChild(newNode, node)
+    })
 }
